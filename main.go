b3852563535e2ee55440143282cf09db6ef9220a
meng.zhu@nav.no <meng.zhu@nav.no>
2024-11-18 01:30:14 +0100

    update

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"log"
 	"path/filepath"
 	"time"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -19,21 +19,42 @@ const schedulerName = "airflow-scheduler"
 const envLocal = "local"
 const envProd = "inCluster"
 
-func getNamespaceFromConfig(kubeconfigPath string) (string, error) {
-	configLoadingRules := &clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeconfigPath}
-	configOverrides := &clientcmd.ConfigOverrides{}
-	clientConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(configLoadingRules, configOverrides)
-	namespace, _, err := clientConfig.Namespace()
-	return namespace, err
+func processNamespace(namespaceName string, deploymentName string, clientset *kubernetes.Clientset) {
+	log.Printf("Processing namespace: %s", namespaceName)
+	deploymentClient := clientset.AppsV1().Deployments(namespaceName)
+
+	deployment, err := deploymentClient.Get(context.Background(), deploymentName, metav1.GetOptions{})
+	if err != nil {
+		if errors.IsNotFound(err) {
+			log.Printf("Deployment %s not found in namespace %s", deploymentName, namespaceName)
+			return
+		} else {
+			log.Printf("Error getting deployment %s in namespace %s: %v", deploymentName, namespaceName, err)
+			return
+		}
+	}
+
+	// Trigger a restart by updating an annotation
+	if deployment.Spec.Template.ObjectMeta.Annotations == nil {
+		deployment.Spec.Template.ObjectMeta.Annotations = make(map[string]string)
+	}
+	deployment.Spec.Template.ObjectMeta.Annotations["knada/restartedAt"] = time.Now().Format(time.RFC3339)
+
+	// Update the deployment
+	_, err = deploymentClient.Update(context.Background(), deployment, metav1.UpdateOptions{})
+	if err != nil {
+		log.Printf("Error updating deployment %s in namespace %s: %v", deploymentName, namespaceName, err)
+		return
+	}
+
+	log.Printf("Deployment %s in namespace %s restarted successfully\n", deploymentName, namespaceName)
 }
 
 func main() {
 	var deploymentName string
-	var namespace string
 	var env string
 
 	flag.StringVar(&deploymentName, "deployment", schedulerName, "Name of the airflow scheduler deployment to restart")
-	flag.StringVar(&namespace, "namespace", "", "Namespace of the airflow scheduler deployment")
 	flag.StringVar(&env, "env", envLocal, "Environment running in")
 	flag.Parse()
 
@@ -45,50 +66,36 @@ func main() {
 			configPath := filepath.Join(home, ".kube", "config")
 			config, err = clientcmd.BuildConfigFromFlags("", configPath)
 			if err != nil {
-				panic(err.Error())
-			}
-			if namespace == "" {
-				namespace, err = getNamespaceFromConfig(configPath)
-				if err != nil {
-					panic(err.Error())
-				}
+				log.Fatalf("Error building kubeconfig: %s", err.Error())
+				return
 			}
 		} else {
-			panic("Home directory not found")
+			log.Fatalf("Home directory not found")
+			return
 		}
 	} else {
 		config, err = rest.InClusterConfig()
 		if err != nil {
-			panic(err.Error())
+			log.Fatalf("Error building in-cluster config: %s", err.Error())
+			return
 		}
 	}
 
-	log.Printf("Restarting airflow scheduler in namespace %s, deploymentName %s\n", namespace, deploymentName)
-
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		panic(err.Error())
+		log.Fatalf("Error creating kubernetes client: %s", err.Error())
+		return
 	}
 
-	deploymentClient := clientset.AppsV1().Deployments(namespace)
-
-	// Get the deployment
-	deployment, err := deploymentClient.Get(context.Background(), deploymentName, metav1.GetOptions{})
+	// Process all namespaces
+	namespaces, err := clientset.CoreV1().Namespaces().List(context.Background(), metav1.ListOptions{})
 	if err != nil {
-		panic(err.Error())
+		log.Fatalf("Error getting namespaces: %s", err.Error())
+		return
 	}
 
-	// Trigger a restart by updating an annotation
-	if deployment.Spec.Template.ObjectMeta.Annotations == nil {
-		deployment.Spec.Template.ObjectMeta.Annotations = make(map[string]string)
+	for _, ns := range namespaces.Items {
+		namespaceName := ns.Name
+		processNamespace(namespaceName, deploymentName, clientset)
 	}
-	deployment.Spec.Template.ObjectMeta.Annotations["knada/restartedAt"] = time.Now().Format(time.RFC3339)
-
-	// Update the deployment
-	_, err = deploymentClient.Update(context.TODO(), deployment, metav1.UpdateOptions{})
-	if err != nil {
-		panic(err.Error())
-	}
-
-	fmt.Printf("Deployment %s in namespace %s restarted successfully\n", deploymentName, namespace)
 }
